189f5fbab3831861c654c35af58cb3514f4b1142
zukarbell <zukarbell@gmail.com>
2024-07-18 15:11:37 +0300

    addded two bit manipualtion functions

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -10,14 +10,25 @@ void VMinitialize()
     }
 }
 
-int isolate_first_bits(uint64_t virtualAddress) {
+uint64_t create_ones_of_len (int len) {
+    return (1ULL << len) - 1;
+}
+
+uint64_t isolate_first_bits(uint64_t virtualAddress) {
     int len_to_move =(int) (WORD_WIDTH / OFFSET_WIDTH);
     len_to_move = len_to_move * OFFSET_WIDTH;
     uint64_t ret_val =  virtualAddress >> len_to_move;
     int delta = WORD_WIDTH % OFFSET_WIDTH;
-    uint64_t delta_bits = (1ULL << delta) - 1;
-    return ret_val & delta_bits;
+    return (ret_val & create_ones_of_len(delta));
     }
+
+uint64_t bit_set_for_level (uint64_t virtualAddress, int tree_lvl) {
+    int num_levels = (int) (WORD_WIDTH / OFFSET_WIDTH);
+    int num_shifts = (num_levels - tree_lvl) * OFFSET_WIDTH;
+    uint64_t ret_val =  virtualAddress >> num_shifts;
+    return (ret_val & create_ones_of_len(OFFSET_WIDTH));
+}
+
 
 int VMread(uint64_t virtualAddress, word_t* value) {
 
