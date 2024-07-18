c3ba7177f04cf5c8262fef1dd659701a533b10ab
zukarbell <zukarbell@gmail.com>
2024-07-18 13:38:10 +0300

    addded isolate_first_bits

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -10,8 +10,21 @@ void VMinitialize()
     }
 }
 
-int VMread(uint64_t virtualAddress, word_t* value){}
+int isolate_first_bits(uint64_t virtualAddress) {
+    int len_to_move =(int) (WORD_WIDTH / OFFSET_WIDTH);
+    len_to_move = len_to_move * OFFSET_WIDTH;
+    uint64_t ret_val =  virtualAddress >> len_to_move;
+    int delta = WORD_WIDTH % OFFSET_WIDTH;
+    uint64_t delta_bits = (1ULL << delta) - 1;
+    return ret_val & delta_bits;
+    }
+
+int VMread(uint64_t virtualAddress, word_t* value) {
+
+}
 
 
 
-int VMwrite(uint64_t virtualAddress, word_t value){}
+int VMwrite(uint64_t virtualAddress, word_t value) {
+
+}
