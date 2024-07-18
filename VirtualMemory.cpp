7213b0927fce2761d2dd17366c34f1a7a3df26d3
dror012Mac <dror012012@gmail.com>
2024-07-18 13:10:53 +0300

    initalize

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,7 +1,14 @@
+#include "VirtualMemory.h"
+#include "PhysicalMemory.h"
+#include "MemoryConstants.h"
 
-void VMinitialize(){}
-
-
+void VMinitialize()
+{
+    for (int i=0; i<PAGE_SIZE;i++)
+    {
+        PMwrite(i, 0);
+    }
+}
 
 int VMread(uint64_t virtualAddress, word_t* value){}
 
